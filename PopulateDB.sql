8cc5986be80a9559dcc94830c0bf86e10a3258d0
Abdulaziz <zarifboev.abdulaziz@gmail.com>
2024-10-31 11:50:45 +0000

    Fixed  DB Connect

---
+++ PopulateDB.sql
@@ -0,0 +1,316 @@
+USE [BookCatalogV5]
+GO
+
+-- Insert data into Categories table 
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 1')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 2')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 3')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 4')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 5')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 6')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 7')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 8')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 9')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 10')
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 11')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 12')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 13')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 14')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 15')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 16')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 17')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 18')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 19')
+GO
+INSERT INTO [dbo].[Categories]
+           ([Name])
+     VALUES
+           ('Catalog 20')
+GO
+
+-- Insert data to Book table
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 1'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 2'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 3'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 4'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 5'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 6'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 7'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 8'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 9'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 10'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 11'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 12'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 13'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 14'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 15'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 16'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 17'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 18'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 19'
+           ,'Book description'
+           ,5)
+GO
+
+INSERT INTO [dbo].[Books]
+           ([Title]
+           ,[Description]
+           ,[CategoryId])
+     VALUES
+           ('Book 20'
+           ,'Book description'
+           ,5)
+GO
